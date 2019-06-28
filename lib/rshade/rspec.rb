3eb58d990e2740bb2610756247f199290c4762aa
Ivan Lopatin <gingray.dev@gmail.com>
2019-06-28 11:49:02 +0200

    add support of RSpec

--- lib/rshade/rspec.rb
+++ lib/rshade/rspec.rb
@@ -1,11 +1,57 @@
-if defined? RSpec
-  RSpec.configure do |c|
-    c.around(:each, rshade: true) do |example|
-      trace = RShade::Trace.new
+module RShade
+  class RSpecListener
+    attr_reader :trace
+
+    NOTIFICATIONS = %i[
+        example_started
+        example_finished
+      ].freeze
+
+
+    def example_started(notification)
+      return unless notification.example.metadata[:rshade]
+
+      @trace = RShade::Trace.new
+      @trace.start
+    end
+
+    def example_finished
+      return unless @trace
+
+      trace.stop
+    end
+
+    def report
+      return unless @trace
+
+      puts trace.show
+    end
+  end
+
+  module RSpecHelper
+    def rshade_reveal(&block)
+      raise 'No block given' unless block_given?
+
+      trace = Trace.new
       trace.reveal do
-        example.run
+        yield
       end
       puts trace.show
     end
+  end
+end
+
+if defined? RSpec
+  RSpec.configure do |c|
+    listener = nil
+    c.include RShade::RSpecHelper
+    c.before(:suite) do
+      listener = RShade::RSpecListener.new
+      c.reporter.register_listener(
+          listener, *RShade::RSpecListener::NOTIFICATIONS
+      )
+    end
+
+    c.after(:suite) { listener.try(:report) }
   end
 end
--- lib/rshade/trace.rb
+++ lib/rshade/trace.rb
@@ -20,6 +20,14 @@ module RShade
       @tp.disable
     end
 
+    def start
+      @tp.enable
+    end
+
+    def stop
+      @tp.disable
+    end
+
     def show
       @source_tree.filter do |node|
         next true unless node.parent
