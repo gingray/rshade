2019b2e748a8a90037204801ccd6ea6d488955d2
Ivan Lopatin <gingray.dev@gmail.com>
2020-06-22 12:31:32 +0200

    remove unnecessary things

--- lib/rshade/cli/cli.rb
+++
@@ -1,3 +0,0 @@
-#TODO
-# is drb works correctly in autoreload process?
-# take a look https://www.slideshare.net/ihower/distributed-ruby-and-rails
--- lib/rshade/cli/interactive.rb
+++
@@ -1,8 +0,0 @@
-require 'drb/drb'
-
-module RShade
-  class Intercative
-    def initialize(host: '', port: '')
-    end
-  end
-end
