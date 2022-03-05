08e2d9bb19d2116cc5cc8a008a314f97c0cd47bf
gingray <gingray.dev@gmail.com>
2022-03-05 19:26:04 +0100

    change config api

--- lib/rshade/config.rb
+++ lib/rshade/config.rb
@@ -1,10 +1,44 @@
-require 'set'
-
 module RShade
   class Config
 
     def self.default
-      ::RShade::Config::Store.new(::RShade::Filter::Default.create, ::RShade::Formatter::Stdout)
+      ::RShade::Config::Store.create(::RShade::Filter::Default.create, ::RShade::Formatter::Stdout)
+    end
+
+    def initialize(config_store)
+      @config_store = config_store
+    end
+
+    def self.create
+      config = new(Config::Store.new)
+    end
+
+    def self.create_with_default
+      config = new(::RShade::Config::Store.create(::RShade::Filter::Default.create, ::RShade::Formatter::Stdout))
+    end
+
+    def include_paths(&block)
+      filter = ::RShade::Filter::IncludePathFilter.new
+      filter.config(&block)
+      @config_store.add_filter(filter)
+      self
+    end
+
+    def exclude_paths(&block)
+      filter = ::RShade::Filter::ExcludePathFilter.new
+      filter.config(&block)
+      @config_store.add_filter(filter)
+      self
+    end
+
+    def formatter(&block)
+      formatter = block.call
+      @config_store.set_formatter(formatter)
+      self
+    end
+
+    def value
+      @config_store
     end
 
     def self.store_dir
--- lib/rshade/config/store.rb
+++ lib/rshade/config/store.rb
@@ -4,13 +4,24 @@ module RShade
     class Store
       attr_reader :filters, :formatter
 
-      def initialize(filters, formatter)
-        @filters = filters.sort_by { |filter| filter.priority }
-        @formatter = formatter
+      def initialize
+        @filters = []
+        @formatter = ::RShade::Formatter::Stdout
       end
 
       def self.create(filters, formatter)
-        new(filters, formatter)
+        new.add_filter(filters).set_formatter(formatter)
+      end
+
+      def add_filter(filters)
+        @filters.append(filters)
+        @filters = @filters.flatten.sort_by { |filter| filter.priority }.reverse
+        self
+      end
+
+      def set_formatter(formatter)
+        @formatter = formatter
+        self
       end
     end
   end
