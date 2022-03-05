287515b7becf92955a2da826711834f6740b0238
gingray <gingray.dev@gmail.com>
2022-03-05 17:12:35 +0100

    WIP

--- lib/rshade.rb
+++ lib/rshade.rb
@@ -21,18 +21,4 @@ require 'rshade/version'
 
 
 module RShade
-  APP_TRACE = :app_trace
-  FULL_TRACE = :full_trace
-
-  class << self
-    attr_writer :config
-
-    def config
-      @config ||= Config.new
-    end
-
-    def configure
-      yield configuration
-    end
-  end
 end
